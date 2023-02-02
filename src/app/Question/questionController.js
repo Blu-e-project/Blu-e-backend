fe86ca23a0ff135ed79a2934c148df5a731c60b0
entere77 <entere77@nate.com>
2023-02-02 16:28:09 +0900

    [MOD] questionController.js, quesitonDao.js, questionProvider.js, eustionRoute.js, questionService.js 파일 필요없는 API 삭제

--- src/app/Question/questionController.js
+++ src/app/Question/questionController.js
@@ -8,16 +8,6 @@ const {response, errResponse} = require("../../../config/response");
 const {emit} = require("nodemon");
 
 /**
- * API No. 0
- * API Name : 테스트 API
- * [GET] /app/test
- */
-exports.getTest = async function (req, res) {
-    return res.send(response(baseResponse.SUCCESS))
-};
-
-
-/**
  * API No. 1
  * API Name : 유저별 작성한 Q&A 조회
  * [GET] /service/questions/:userId
@@ -70,63 +60,8 @@ exports.postQuestion = async function (req, res) {
 };
 
 
-
 /**
  * API No. 3
- * API Name : Question 수정
- * [PATCH] /service/questions/:userId/writing/:questionId
- * path variable : questionId
- * body : title, contents
- */
-exports.patchQuestion = async function (req, res) {
-
-    const userId = req.params.userId;
-    const questionId = req.params.questionId;
-
-    const title = req.body.title;
-    const contents = req.body.contents;
-
-    // 빈 값 체크
-    if (!userId) return res.send('오류 발생');
-
-    // 빈 값 체크
-
-    if (!questionId) return res.send('오류 발생');
-
-    // 빈 값 체크
-    if (!title)
-        return res.send('제목이 비어있습니다.');
-
-    // 빈 값 체크
-    if (!contents)
-        return res.send('글내용이 비어있습니다.');
-
-    const updateQuestionResponse = await questionService.editQuestion(
-        title,
-        contents,
-        userId,
-        questionId,
-    );
-
-    return res.send(updateQuestionResponse);
-};
-
-
-/**
- * API No. 4
- * API Name : 답변이 필요한 질문 조회
- * [GET] /service/questions
- */
-exports.getQuestionNeedingAnswer = async function (req, res) {
-
-    const QuestionNeedingAnswer = await questionProvider.retrieveQuestionNeedingAnswer();
-    return res.send(response(baseResponse.SUCCESS, QuestionNeedingAnswer));
-};
-
-
-
-/**
- * API No. 5
  * API Name : Question 삭제 API
  * [DELETE] /service/questions/:userId/writing/:questionId
  */
